e9d9f5bcee8b19efb4581a39b5f8ec2c350e46cb
umar.ramadhana <umar.ramadhana@gmail.com>
2022-04-15 12:59:56 +0700

    raw error, mangabat chapter fix and image proxy

--- internal/services/manga_scrapper/mangabat.go
+++ internal/services/manga_scrapper/mangabat.go
@@ -216,9 +216,9 @@ func GetMangabatDetailChapter(ctx context.Context, queryParams models.QueryParam
 		chapter.ChapterImages = append(chapter.ChapterImages, models.ChapterImage{
 			Index: 0,
 			ImageUrls: []string{
-				fmt.Sprintf("http://localhost:6001/mangas/mangabat/image_proxy/%v", e.Attr("src")),
 				fmt.Sprintf("https://animapu-api.herokuapp.com/mangas/mangabat/image_proxy/%v", e.Attr("src")),
 				fmt.Sprintf("https://go-animapu.herokuapp.com/image_proxy/%v", e.Attr("src")),
+				fmt.Sprintf("http://localhost:6001/mangas/mangabat/image_proxy/%v", e.Attr("src")),
 			},
 		})
 	})
