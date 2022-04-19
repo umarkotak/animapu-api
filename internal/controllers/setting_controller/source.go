5a0b7496402dfec0b68168596847fb230a9877cd
umar.ramadhana <umar.ramadhana@gmail.com>
2022-04-19 17:17:37 +0700

    fomalize source

--- internal/controllers/setting_controller/source.go
+++ internal/controllers/setting_controller/source.go
@@ -5,17 +5,61 @@ import (
 	"github.com/umarkotak/animapu-api/internal/utils/render"
 )
 
+type MangaSource struct {
+	ID       string `json:"id"`
+	Language string `json:"language"`
+	Title    string `json:"title"`
+	WebLink  string `json:"web_link"`
+	Active   bool   `json:"active"`
+}
+
 func GetAvailableSource(c *gin.Context) {
 	render.Response(
 		c.Request.Context(),
 		c,
-		[]string{
-			"mangabat",
-			"mangaupdates",
-			// "mangadex",
-			// "maidmy",
-			// "klikmanga",
-			// "mangareadorg",
+		[]MangaSource{
+			{
+				ID:       "mangabat",
+				Language: "en",
+				Title:    "Manga Bat",
+				WebLink:  "https://m.mangabat.com/m",
+				Active:   true,
+			},
+			{
+				ID:       "mangaupdates",
+				Language: "en",
+				Title:    "Manga Updates",
+				WebLink:  "https://www.mangaupdates.com/",
+				Active:   true,
+			},
+			{
+				ID:       "mangadex",
+				Language: "mix",
+				Title:    "Manga Dex",
+				WebLink:  "https://mangadex.org/",
+				Active:   false,
+			},
+			{
+				ID:       "maidmy",
+				Language: "id",
+				Title:    "Maid My",
+				WebLink:  "https://www.maid.my.id/",
+				Active:   false,
+			},
+			{
+				ID:       "klikmanga",
+				Language: "id",
+				Title:    "Klik Manga",
+				WebLink:  "https://klikmanga.id/",
+				Active:   false,
+			},
+			{
+				ID:       "mangaread",
+				Language: "en",
+				Title:    "Manga Read",
+				WebLink:  "https://www.mangaread.org/",
+				Active:   false,
+			},
 		},
 		nil,
 		200,
