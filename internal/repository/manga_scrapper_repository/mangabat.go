0cfcb9713b77fe2e205b4531c09a72cfcd9ef4d3
umar.ramadhana <umar.ramadhana@everywhere.id>
2024-02-06 12:15:23 +0700

    improve mangabat

--- internal/repository/manga_scrapper_repository/mangabat.go
+++ internal/repository/manga_scrapper_repository/mangabat.go
@@ -14,12 +14,14 @@ import (
 )
 
 type Mangabat struct {
-	Host string
+	Host     string
+	ReadHost string
 }
 
 func NewMangabat() Mangabat {
 	return Mangabat{
-		Host: "https://h.mangabat.com",
+		Host:     "https://h.mangabat.com",
+		ReadHost: "https://readmangabat.com",
 	}
 }
 
@@ -32,30 +34,27 @@ func (m *Mangabat) GetHome(ctx context.Context, queryParams models.QueryParams) 
 
 	c := colly.NewCollector()
 	c.SetRequestTimeout(config.Get().CollyTimeout)
-	// t := &http.Transport{
-	// 	Dial: (&net.Dialer{
-	// 		Timeout:   60 * time.Second,
-	// 		KeepAlive: 30 * time.Second,
-	// 	}).Dial,
-	// 	TLSHandshakeTimeout: 60 * time.Second,
-	// }
-	// c.WithTransport(t)
 
 	c.OnHTML("body div.body-site div.container.container-main div.container-main-left div.panel-list-story .list-story-item", func(e *colly.HTMLElement) {
-		sourceID := strings.Replace(e.ChildAttr("div > h3 > a", "href"), "https://read.mangabat.com/", "", -1)
-		sourceID = strings.Replace(sourceID, "https://m.mangabat.com/", "", -1)
-		sourceID = strings.Replace(sourceID, "https://readmangabat.com/", "", -1)
-		sourceID = strings.Replace(sourceID, "https://h.mangabat.com/", "", -1)
+		sourceID := ""
+		mangaLink := e.ChildAttr("div > h3 > a", "href")
+		mangaLinkSplitted := strings.Split(mangaLink, "/")
+		if len(mangaLinkSplitted) > 0 {
+			sourceID = mangaLinkSplitted[len(mangaLinkSplitted)-1]
+		}
 
-		imageURL := e.ChildAttr("a > img", "src")
+		latestChapterID := ""
+		latestChapterLink := e.ChildAttr("div > a:nth-child(2)", "href")
+		mangaLinkSplittedSplitted := strings.Split(latestChapterLink, "/")
+		if len(mangaLinkSplittedSplitted) > 0 {
+			latestChapterID = mangaLinkSplittedSplitted[len(mangaLinkSplittedSplitted)-1]
+		}
 
 		latestChapterText := e.ChildText("div > a:nth-child(2)")
-		latestChapterID := strings.Replace(e.ChildAttr("div > a:nth-child(2)", "href"), "https://read.mangabat.com/", "", -1)
-		latestChapterID = strings.Replace(latestChapterID, "https://m.mangabat.com/", "", -1)
-		latestChapterID = strings.Replace(latestChapterID, "https://readmangabat.com/", "", -1)
-
 		latestChapterNumberString := strings.Replace(latestChapterText, "Chapter ", "", -1)
 		latestChapterNumber, _ := strconv.ParseFloat(latestChapterNumberString, 64)
+
+		imageURL := e.ChildAttr("a > img", "src")
 
 		if latestChapterNumber == 0 {
 			latestChapterNumberSplitted := strings.Split(latestChapterID, "-")
@@ -131,10 +130,6 @@ func (m *Mangabat) GetDetail(ctx context.Context, queryParams models.QueryParams
 	idx := int64(1)
 	c.OnHTML("body > div.body-site > div.container.container-main > div.container-main-left > div.panel-story-chapter-list > ul > li", func(e *colly.HTMLElement) {
 		chapterLink := e.ChildAttr("a", "href")
-		chapterLink = strings.Replace(chapterLink, "https://read.mangabat.com/", "", -1)
-		chapterLink = strings.Replace(chapterLink, "https://m.mangabat.com/", "", -1)
-		chapterLink = strings.Replace(chapterLink, "https://h.mangabat.com/", "", -1)
-
 		splittedLink := strings.Split(chapterLink, "-")
 		chapterNumber, _ := strconv.ParseFloat(splittedLink[len(splittedLink)-1], 64)
 		id := fmt.Sprintf("chap-%v", splittedLink[len(splittedLink)-1])
@@ -151,23 +146,8 @@ func (m *Mangabat) GetDetail(ctx context.Context, queryParams models.QueryParams
 		idx += 1
 	})
 
-	err := c.Visit(fmt.Sprintf("https://m.mangabat.com/%v", queryParams.SourceID))
+	err := c.Visit(fmt.Sprintf("%s/%v", m.ReadHost, queryParams.SourceID))
 	c.Wait()
-
-	if manga.Title == "" {
-		err = c.Visit(fmt.Sprintf("https://read.mangabat.com/%v", queryParams.SourceID))
-		c.Wait()
-	}
-
-	if manga.Title == "" {
-		err = c.Visit(fmt.Sprintf("https://readmangabat.com/%v", queryParams.SourceID))
-		c.Wait()
-	}
-
-	if manga.Title == "" {
-		err = c.Visit(fmt.Sprintf("%s/%v", m.Host, queryParams.SourceID))
-		c.Wait()
-	}
 
 	if err != nil {
 		logrus.WithContext(ctx).Error(err)
@@ -186,11 +166,12 @@ func (m *Mangabat) GetSearch(ctx context.Context, queryParams models.QueryParams
 	c.SetRequestTimeout(config.Get().CollyTimeout)
 
 	c.OnHTML("body > div.body-site > div.container.container-main > div.container-main-left > div.panel-list-story > div", func(e *colly.HTMLElement) {
-		detailUrl := e.ChildAttr("a.item-img", "href")
-		sourceID := strings.Replace(detailUrl, "https://read.mangabat.com/", "", -1)
-		sourceID = strings.Replace(sourceID, "https://m.mangabat.com/", "", -1)
-		sourceID = strings.Replace(sourceID, "https://readmangabat.com/", "", -1)
-		sourceID = strings.Replace(sourceID, "https://h.mangabat.com/", "", -1)
+		sourceID := ""
+		mangaLink := e.ChildAttr("a.item-img", "href")
+		mangaLinkSplitted := strings.Split(mangaLink, "/")
+		if len(mangaLinkSplitted) > 0 {
+			sourceID = mangaLinkSplitted[len(mangaLinkSplitted)-1]
+		}
 
 		title := e.ChildText("div > h3 > a")
 
@@ -261,10 +242,7 @@ func (m *Mangabat) GetChapter(ctx context.Context, queryParams models.QueryParam
 
 	var err error
 	targets := []string{
-		fmt.Sprintf("https://m.mangabat.com/%v-%v", queryParams.SourceID, queryParams.ChapterID),
-		fmt.Sprintf("https://read.mangabat.com/%v-%v", queryParams.SourceID, queryParams.ChapterID),
-		fmt.Sprintf("https://readmangabat.com/%v-%v", queryParams.SourceID, queryParams.ChapterID),
-		fmt.Sprintf("https://h.mangabat.com/%v-%v", queryParams.SourceID, queryParams.ChapterID),
+		fmt.Sprintf("%s/%v-%v", m.ReadHost, queryParams.SourceID, queryParams.ChapterID),
 	}
 
 	for _, targetLink := range targets {
@@ -274,7 +252,7 @@ func (m *Mangabat) GetChapter(ctx context.Context, queryParams models.QueryParam
 			strings.NewReader("{}"),
 			colly.NewContext(),
 			http.Header{
-				"Authority":                 []string{"readmangabat.com"},
+				"Authority":                 []string{strings.ReplaceAll(m.ReadHost, "https://", "")},
 				"Accept":                    []string{"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"},
 				"Accept-Language":           []string{"en-US,en;q=0.9,id;q=0.8"},
 				"Cache-Control":             []string{"max-age=0"},
